329bb620c29e6e3a8f12d87eba9bc012016a086d
minjysuh <bt09037@gmail.com>
2024-04-24 12:58:07 +0900

    beam

--- Ultrasound/main.py
+++ Ultrasound/main.py
@@ -1,1 +1,16 @@
 import numpy as np
+from matplotlib import pyplot as plt
+
+
+rf_data = np.load('arm_rfdata.npy')
+
+def das_beamforming(rf_data, c, fs, pitch, element_pos, image_size, image_res):
+    # rf_data: RF data
+    # c: speed of sound
+    # fs: sampling frequency
+    # pitch: distance between elements
+    # element_pos: position of elements
+    # image_size: size of image
+    # image_res: resolution of image
+
+    return image
